031cba4a67be6006b635ce9cd2185759d4ad57eb
Caroline Malmgren <caroline.malmgren@live.se>
2018-11-14 16:57:39 +0100

    Added drawing functionality. Also added scoring functionality, but this is not yet shown

--- FunctionalityWithJQuery.js
+++ FunctionalityWithJQuery.js
@@ -1,5 +1,106 @@
 ﻿$(function () {
 
+    let paused = false;
+    let playerHand = [];
+    let computerHand = [];
+    let currentComputerCard = undefined;
+    let currentPlayerCard = undefined;
+
+    var cards = $("section.cards");
+    cards.hide();
+
+    var newGameButton = $("#newGame");
+    newGameButton.click(function () {
+        newGameButton.addClass("clicked");
+        cards.show();
+
+        var sec = 0;
+        function pad(val) { return val > 9 ? val : "0" + val; }
+        setInterval(function () {
+            $("#seconds").html(pad(++sec % 60));
+            $("#minutes").html(pad(parseInt(sec / 60, 10)));
+        }, 1000);
+
+        $.getJSON("https://deckofcardsapi.com/api/deck/new/draw/", { count: 52 })
+            .done(function (data) {
+                console.log(data);
+
+                let index = 0;
+                data.cards.forEach(function (card) {
+
+                    if (index % 2 == 0) {
+                        playerHand.push(card);
+                        index++;
+                    }
+                    else {
+                        computerHand.push(card);
+                        index++;
+                    }
+
+                    SetPlayerCard(currentPlayerCard);
+                });
+            })
+            .fail(function () {
+                console.log("This is broken");
+            });
+    });
+
+    function ComputerDrawsCard(computerHand, CardDrawnFunction) {
+        //setTimeout(function () {
+        CardDrawnFunction(computerHand.pop());
+        /*}, Math.floor(Math.random() * 6000) + 3000);*/  //3-6 sekunder
+    }
+
+    setInterval(function () {
+        ComputerDrawsCard(computerHand, function (theCard) {
+            currentComputerCard = theCard;
+            if (currentComputerCard !== undefined) {
+                $("#ComputerCard img").attr("src", theCard.image);
+            }
+        });
+    }, Math.floor(Math.random() * 6000) + 3000); //3-6 sekunder
+
+
+    var pauseButton = $("#pause");
+    pauseButton.click(function () {
+        //timer pause  if pause is false så kan saker köras. ändra om pauseknappen till play när den är intryckt
+        //disable ability to play cards
+    });
+
+    var playerCard = $("#PlayerCard");
+    playerCard.click(function () {
+        if (currentComputerCard !== undefined && currentPlayerCard !== undefined) {
+            ScorePoints(currentPlayerCard, currentComputerCard)
+        }
+
+        SetPlayerCard(playerHand.pop())
+        let newEl = $("<strong> Played! </strong>");
+        playerCard.append(newEl);
+        newEl.hide(6000);
+
+    });
+
+    function SetPlayerCard(card) {
+
+        currentPlayerCard = card;
+        if (currentPlayerCard !== undefined) {
+            $("#PlayerCard img").attr("src", card.image);
+        }
+    }
+
+    function ScorePoints(card1, card2) {
+        if (card1.value === card2.value || card1.suit === card2.suit) {
+            score += 1;
+        }
+        else score -= 1;
+
+    }
+});
+
+//ToDo: Implementera pausen, visa upp score dynamiskt
+
+//To future Carro: For sanity, keep these as examples....
+
     //$.getJSON("https://deckofcardsapi.com/api/deck/new/shuffle/", { deck_count: 1 })
     //    .done(function (data) {
     //        console.log(data);
@@ -23,68 +124,3 @@
     //    .fail(function () {
     //        console.log("broken");
     //    });
-
-    var cards = $("section.cards");
-    cards.hide();
-
-    var newGameButton = $("#newGame");
-    newGameButton.click(function () {
-        newGameButton.addClass("clicked");
-        cards.show();
-
-        //enable playing cards for real at later stage
-
-        var sec = 0;
-        function pad(val) { return val > 9 ? val : "0" + val; }
-        setInterval(function () {
-            $("#seconds").html(pad(++sec % 60));
-            $("#minutes").html(pad(parseInt(sec / 60, 10)));
-        }, 1000);
-
-        $.getJSON("https://deckofcardsapi.com/api/deck/new/draw/", { count: 52 })
-            .done(function (data) {
-                console.log(data);
-                let playerHand = [];
-                let computerHand = [];
-
-                let index = 0;
-                data.cards.forEach(function (card) {
-
-                    if (index % 2 == 0) {
-                        playerHand.push(card);
-                        index++;
-                    }
-                    else {
-                        computerHand.push(card);
-                        index++;
-                    }
-                });
-
-                playerHand.forEach(function (card) {
-                    $("section.cards").append($("<img src= '" + card.image + "'>")
-                    );
-                });
-            })
-            .fail(function () {
-                console.log("broken");
-            });
-    });
-
-    $('button'/*döp om till playercard*/).click(function () {
-        //implementera spelandet av kortet
-        //
-    });
-
-    var pauseButton = $("#pause");
-    pauseButton.click(function () {
-        //timer pause
-        //disable ability to play cards
-    });
-
-    var playerCard = $("#PlayerCard");
-    playerCard.click(function () {
-        let newEl = $("<strong>Played! </strong>");
-        playerCard.append(newEl);
-        newEl.hide(6000);
-    });
-});
--- JavaScript.js
+++ JavaScript.js
@@ -1,4 +1,5 @@
-﻿function CreateDeck() {
+﻿
+function CreateDeck() {
 
     const cardValues = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
     const suitValues = ['h', 'c', 'd', 's'];
