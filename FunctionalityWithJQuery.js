c92fe5a78087f22847e17ab040db858595770609
Caroline Malmgren <caroline.malmgren@live.se>
2018-11-14 14:46:38 +0100

    added card api, now player starts with 26 cards which are shown on screen. computer also has 26 cards.

--- FunctionalityWithJQuery.js
+++ FunctionalityWithJQuery.js
@@ -1,5 +1,29 @@
 ﻿$(function () {
 
+    //$.getJSON("https://deckofcardsapi.com/api/deck/new/shuffle/", { deck_count: 1 })
+    //    .done(function (data) {
+    //        console.log(data);
+    //        console.log(data.remaining);
+    //    })
+    //    .fail(function () {
+    //        console.log("broken");
+    //    });
+
+    //let deckID = "v9nakxwl741h";
+    //let url = "https://deckofcardsapi.com/api/deck/" + deckID + "/draw/";
+    //$.getJSON(url, { count: 26 })
+    //    .done(function (data) {
+    //        console.log(data);
+
+    //        data.cards.forEach(function (card) {
+    //            $("section.cards").append($("<img src= '" + card.image + "'>")
+    //            );
+    //        });
+    //    })
+    //    .fail(function () {
+    //        console.log("broken");
+    //    });
+
     var cards = $("section.cards");
     cards.hide();
 
@@ -7,8 +31,8 @@
     newGameButton.click(function () {
         newGameButton.addClass("clicked");
         cards.show();
-        
-        //enable playing cards
+
+        //enable playing cards for real at later stage
 
         var sec = 0;
         function pad(val) { return val > 9 ? val : "0" + val; }
@@ -16,6 +40,39 @@
             $("#seconds").html(pad(++sec % 60));
             $("#minutes").html(pad(parseInt(sec / 60, 10)));
         }, 1000);
+
+        $.getJSON("https://deckofcardsapi.com/api/deck/new/draw/", { count: 52 })
+            .done(function (data) {
+                console.log(data);
+                let playerHand = [];
+                let computerHand = [];
+
+                let index = 0;
+                data.cards.forEach(function (card) {
+
+                    if (index % 2 == 0) {
+                        playerHand.push(card);
+                        index++;
+                    }
+                    else {
+                        computerHand.push(card);
+                        index++;
+                    }
+                });
+
+                playerHand.forEach(function (card) {
+                    $("section.cards").append($("<img src= '" + card.image + "'>")
+                    );
+                });
+            })
+            .fail(function () {
+                console.log("broken");
+            });
+    });
+
+    $('button'/*döp om till playercard*/).click(function () {
+        //implementera spelandet av kortet
+        //
     });
 
     var pauseButton = $("#pause");
@@ -30,6 +87,4 @@
         playerCard.append(newEl);
         newEl.hide(6000);
     });
-
-    
 });
