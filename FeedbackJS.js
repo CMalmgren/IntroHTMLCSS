9fb27a9757171deb960b6c43578525152fed8428
Caroline Malmgren <caroline.malmgren@live.se>
2018-11-15 13:30:01 +0100

    added feedback

---
+++ FeedbackJS.js
@@ -0,0 +1,27 @@
+﻿$(function () {
+    $('#theForm').submit(function (e) {
+        e.preventDefault();
+
+        let textInput = $('#textID');
+        let happinessInput = $('input[name=happiness]:checked');
+        let colorInput = $('#colorID');
+        let newRow = $('<tr>');
+        let textCell = $('<td>');
+        let happinessCell = $('<td>');
+        let colorCell = $('<td>');
+
+        textCell.text(textInput.val());
+        happinessCell.text(happinessInput.val())
+        colorCell.text(colorInput.val());
+        colorCell.css('background-color', colorInput.val());
+
+        newRow.append(textCell);
+        newRow.append(happinessCell);
+        newRow.append(colorCell);
+        $('#ResultBody').append(newRow);
+
+
+    });
+        
+       
+});
